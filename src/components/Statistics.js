3beaf42afb8e06d2c38943fc4bc026b5bc4d48ed
TetianaPiatykh <tanya.pjatykh@gmail.com>
2022-10-09 11:40:32 +0300

    0910221140

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -6,6 +6,7 @@ export const Statistics = ({good, neutral, bad, total, positivePercentage }) => 
             <li>Good: {good}</li>
             <li>Neutral: {neutral}</li>
             <li>Bad: {bad}</li>
+            <li>Total: {total}</li>
             <li>Positive feedback: {positivePercentage} %</li>
         </StatisticsList>)
 };
